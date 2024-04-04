21121c6c403daebdcedfea83d0f14036683660dc
dw <itscomputers@proton.me>
2024-04-04 14:41:56 -0600

    add api base url from environment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,11 @@
 const api = (token) => {
+  // generally, you access environment using `process.env`
+  // in Vite, you access via `import.meta.env`
+  //    - by default only gives access to env variables that
+  //      are prefixed with `VITE_`
+  // we can access env variables defined outside (eg export VITE_VAR=hi)
+  // or defined in a .env file
+
   const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
   const headers = {
@@ -9,10 +16,12 @@ const api = (token) => {
     headers["Authorization"] = "Bearer " + token;
   }
 
+  // get("/users/me") ~> finish the same way we would with fetch
   const get = (url) => (
-    fetch(baseUrl + url, { method: "GET", headers, })
+    fetch(baseUrl + url, { method: "GET", headers })
   );
 
+  // post("/auth/registration", { username, email, password })
   const post = (url, body) => (
     fetch(
       baseUrl + url,
@@ -24,6 +33,7 @@ const api = (token) => {
     )
   );
 
+  // postForm("/auth/token", { username, password })
   const postForm = (url, body) => (
     fetch(
       baseUrl + url,
